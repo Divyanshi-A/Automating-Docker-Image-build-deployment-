3d7cba9cfff60b4bea050233b51445c3d29ef42d
Divyanshi-A <113288419+Divyanshi-A@users.noreply.github.com>
2024-10-03 16:28:06 +0530

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,26 @@
 const request = require('supertest');
+const express = require('express');
 const app = require('../index');
 
 describe('Stopwatch App', () => {
+  let server;
+
+  beforeAll((done) => {
+    server = express().use(app);
+    server.listen(8080, done);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('should return a 200 status code for the root endpoint', async () => {
-    const response = await request(app).get('/');
+    const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
   });
 
   it('should start the stopwatch', async () => {
-    const response = await request(app).get('/start');
+    const response = await request(server).get('/start');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Stopwatch started.');
   });
