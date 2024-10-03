12f7b807f5bdef64ac0c913485aed2658bbdd2cd
Divyanshi-A <113288419+Divyanshi-A@users.noreply.github.com>
2024-10-03 16:22:23 +0530

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,5 @@
 const request = require('supertest');
 const app = require('../index');
-let server;
-
-beforeAll(() => {
-  server = app.listen(8080);
-});
-
-afterAll((done) => {
-  server.close(done);
-});
 
 describe('Stopwatch App', () => {
   it('should return a 200 status code for the root endpoint', async () => {
